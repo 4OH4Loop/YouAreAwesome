4e762b62be1480ff3fdb2e83dd99eb563abff37a
4OH4Loop <semperchuck@gmail.com>
2025-01-25 08:51:00 -0500

    Ch 1.10 Updates

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,33 +12,35 @@ struct ContentView: View {
     @State private var imageName = ""
     @State private var imageNumber = 0
     @State private var messageNumber = 0
+    
     var body: some View {
         
         VStack {
-            
-            Spacer()
+              Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+                .animation(.default, value: imageName)
             
             Spacer()
     
             Button("Show Message") {
                 let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
                                 "You Are Great!",
                                 "Fabulous? That's You!",
                                 "You Are a Code Monster!",
-                                "You Make Me Smile!",
-                                "When the Genius Bar Needs Help, They Call You!"]
+                                "You Make Me Smile!"]
                 
                 message = messages[messageNumber]
                 messageNumber += 1
